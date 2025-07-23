960801589cab8c891cc5b79804ab9ae9d2549237
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-07-22 19:27:10 -0700

    Added tailwind cdn

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/healthz')
 def healthz():
     return "OK", 200
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
